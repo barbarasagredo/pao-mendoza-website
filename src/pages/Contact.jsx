e20c8c9887ada725f8910522ad800bdad091a2f6
Bárbara Sagredo <barpaz.sc@gmail.com>
2023-04-19 23:02:19 -0400

    contact page created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,82 @@
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 const Contact = () => {
-    return (
-      <>
-        <div>
-          Contact
-        </div>
-      </>
-    );
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_f7mxomb',
+        'contact_form',
+        form.current,
+        'CqdMPnR641yxueMAp'
+      )
+      .then(
+        (result) => {
+          alert('Correo enviado correctamente');
+        },
+        (error) => {
+          alert(`Ocurrio un error, intente nuevamente - ${error}`);
+        }
+      );
+    e.target.reset();
   };
-  
-  
-  export default Contact;
+
+  return (
+    <>
+      <div className='flex flex-col items-center w-full min-h-screen pt-16'>
+        <div className='w-2/3 text-center space-y-2 pb-4'>
+          <h2>CONTACT ME</h2>
+          <p className='text-sm'>
+            If you are interested in my work and want to know more about me or
+            my projects, you can find me in my social media or send me a
+            message. Let’s talk.
+          </p>
+        </div>
+        <form ref={form} onSubmit={sendEmail} className='w-1/2'>
+          <div className='space-y-1 pt-4'>
+            <label className='block text-sm'>Enter your name</label>
+            <input
+              className='border text-sm bg-transparent border-gray-400 w-full p-2 focus:outline-none focus:shadow-outline'
+              type='text'
+              name='user_name'
+              required
+              placeholder='Write your name'
+            />
+          </div>
+          <div className='space-y-1 pt-4'>
+            <label className='block text-sm'>Enter your mail</label>
+            <input
+              className='border text-sm bg-transparent border-gray-400 w-full p-2 focus:outline-none focus:shadow-outline'
+              type='text'
+              name='user_email'
+              required
+              placeholder='Write your email'
+            />
+          </div>
+          <div className='space-y-1 pt-4'>
+            <label className='block text-sm'>Leave a message</label>
+            <textarea
+              className='border text-sm bg-transparent border-gray-400 w-full p-2 focus:outline-none focus:shadow-outline'
+              rows={4}
+              type='text'
+              name='message'
+              required
+              placeholder='Leave a message'
+            />
+          </div>
+        </form>
+        <button
+          className='border border-gray-500 rounded-md w-24 h-12 hover:bg-gray-400 hover:text-white text-sm my-4'
+          type='submit'
+        >
+          Send
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
