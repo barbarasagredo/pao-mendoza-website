170f35dc949959e247e26e64546e411bd778eb7f
Bárbara Sagredo <barpaz.sc@gmail.com>
2023-04-20 01:33:02 -0400

    Not found page created

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <>
-      <div>
-        <h1>404</h1>
+      <div className='flex flex-col items-center justify-center w-full min-h-screen space-y-3'>
+        <h1 className='text-8xl'>404</h1>
         <h2>Page not found</h2>
         <p>
           Sorry, the page you're looking for doesn't exist or is unavailable.
         </p>
-        <Link to={'/'}>Go back</Link>
+        <span className='border border-gray-500 rounded-md w-24 h-11 hover:bg-gray-400 hover:text-white text-sm flex items-center justify-center'>
+          <Link to={'/'}>Go back</Link>
+        </span>
       </div>
     </>
   );
